a6437c5d0d209a263349c4aefcfca5f09fb30fdd
yash987 <ys2599518@gmail.com>
2025-07-27 12:55:40 +0530

    final

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,7 +1,7 @@
 import { atom } from 'recoil';
 import type { User } from '../utils';
 
-export const UserAtom = atom({
+export const UserAtom = atom<User>({
 	key: 'User',
 	default: localStorage.getItem('user-info')
 		? JSON.parse(localStorage.getItem('user-info')!)
