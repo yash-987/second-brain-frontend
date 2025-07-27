0c8e505540d80baf83d4972fb1b1189003b1a4f1
yash987 <ys2599518@gmail.com>
2025-07-27 13:02:49 +0530

    changed routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<Navigate to="/signin" replace />} />
+				<Route path="/" element={<Navigate to="/signup" replace />} />
 				<Route path="/signup" element={<Signup />} />
 				<Route path="/signin" element={<Signin />} />
 				<Route path="/dashboard" element={<Dashboard />} />
-				<Route path="*" element={<Navigate to="/signin" replace />} />
+				<Route path="*" element={<Navigate to="/signup" replace />} />
 			</Routes>
 
 			<ToastContainer
